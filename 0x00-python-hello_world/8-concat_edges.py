98fb2424dbe84e6a227cac3693d3f04a5fe79829
Ismael Tech <133222922+IsmaelKiprop@users.noreply.github.com>
2023-09-04 22:29:13 -0700

    Update 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "object-oriented programming"
-str2 = " with Python"
-str1 = str1[:27]
-print(str1 + str2)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[-22:-17] + str[0:6]
+print(str)
