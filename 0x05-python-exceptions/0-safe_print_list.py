a410f710c0389d36385c4f9bdf2328ef0f49decf
IsmaelKiprop <kipropismael27.com>
2023-09-25 22:56:30 -0700

    exeption

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    count = 0  # Initialize a counter for the elements printed
-    
-    try:
-        for i in range(x):
-            print("{}".format(my_list[i]), end="")  # Print the element without a newline
-            count += 1
-    except IndexError:  # Handle the exception when the index is out of range
-        pass
-    
-    print()  # Print a newline to separate the printed elements from the next output
-    return count  # Return the number of elements printed
+    """Print x elememts of a list.
 
-# Test cases
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
 
-    nb_print = safe_print_list(my_list, 2)
-    print("nb_print: {:d}".format(nb_print))
-
-    nb_print = safe_print_list(my_list, len(my_list))
-    print("nb_print: {:d}".format(nb_print))
-
-    nb_print = safe_print_list(my_list, len(my_list) + 2)
-    print("nb_print: {:d}".format(nb_print))
+    Returns:
+        The number of elements printed.
+    """
+    ret = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            ret += 1
+        except IndexError:
+            break
+    print("")
+    return (ret)
