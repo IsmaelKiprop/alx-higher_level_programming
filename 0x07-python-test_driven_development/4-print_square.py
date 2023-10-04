ac9a88fa25d283737021bfdf0cebf6e542311827
Ismael Tech <133222922+IsmaelKiprop@users.noreply.github.com>
2023-10-04 10:27:03 +0300

    Update 4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -3,16 +3,16 @@
 
 
 def print_square(size):
-        """Prints a square to user-defined dimensions
+    """Prints a square to user-defined dimensions
 
-            Non-integers and arguments less than 0 will raise errors.
+    Non-integers and arguments less than 0 will raise errors.
 
-                Args:
-                        size: dimensions of square
-                            """
-                                if type(size) is not int:
-                                            raise TypeError('size must be an integer')
-                                            if size < 0:
-                                                        raise ValueError('size must be >= 0')
-                                                        for row in range(size):
-                                                                    print('#' * size)
+    Args:
+        size: dimensions of square
+    """
+    if type(size) is not int:
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    for row in range(size):
+        print('#' * size)
