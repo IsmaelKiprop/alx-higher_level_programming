9751821a5468d077ae3b4a229ded388263ce5aa8
Ismael Tech <133222922+IsmaelKiprop@users.noreply.github.com>
2023-10-17 21:22:35 +0300

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,152 +1,170 @@
 #!/usr/bin/python3
-"""Contain class `Rectangle` that inherits from `Base` """
-
-
+"""Defines a rectangle module (modules.rectangle)"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """
-    Class `Rectangle` that inherits from `Base`:
-
-    In the file models/rectangle.py
-    Class Rectangle inherits from Base
-    Private instance attributes, each with its own public getter and setter:
-    __width -> width
-    __height -> height
-    __x -> x
-    __y -> y
-    Class constructor: def __init__(self, width, height, x=0, y=0, id=None):
-    Call the super class with id - this super call with use the logic
-    of the __init__ of the Base class
-    Assign each argument width, height, x and y to the right attribute
-    """
+    """Defines a rectangle class"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Instantiate class object.
-        Arguments
-            width: width of rectangle
-            height: height of rectangle
-            x: x axis offset
-            y: y axis offset
-            id: object id
+        """Method that init values for a rectangle object
+        Args:
+           width:size of the width
+           height: size of the height
+           x: Variable x
+           y:  Variable y
+        Return:
+           Always nothing
         """
-
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
-    def display(self):
-        """Display the rectangle by width, height, x and y"""
-        for i in range(self.y):
-            print("")
-        for j in range(self.height):
-            for k in range(self.x):
-                print(" ", end='')
-            for k in range(self.width):
-                print("#", end='')
-            print()
-
-    def area(self):
-        """Return area value of Rectangle"""
-        return self.width * self.height
-
+    # Getter and setter of width
     @property
     def width(self):
-        """Return value of width"""
+        """Getter the size of width
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Set the value for width"""
+        """Setter the size of width
+        Args:
+           value: Size to assign to the width
+        Return:
+           Always Nothing
+        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("width must be > 0")
-        self.__width = value
+        else:
+            self.__width = value
 
+    # Getter and setter of height
     @property
     def height(self):
-        """Return value of height"""
+        """Getter the size of height
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Set the value for height"""
+        """Setter the size of height
+        Args:
+           value: Size to assign to the height
+        Return:
+           Always Nothing
+        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("height must be > 0")
-        self.__height = value
+        else:
+            self.__height = value
 
+    # Getter and setter for x variable
     @property
     def x(self):
-        """Return value of x"""
+        """Getter of x variable
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
-        """Set the value for x"""
+        """Setter of x variable
+        Args:
+           value: value to assign to x variable
+        Return:
+           Always Nothing
+        """
         if type(value) is not int:
             raise TypeError("x must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("x must be >= 0")
-        self.__x = value
+        else:
+            self.__x = value
 
+    # Getter and setter for y variable
     @property
     def y(self):
-        """Return value of y"""
+        """Getter of y variable
+        """
         return self.__y
 
     @y.setter
     def y(self, value):
-        """Set the value for y"""
+        """Setter of y variable
+        Args:
+           value: value to assign to y variable
+        Return:
+           Always Nothing
+        """
         if type(value) is not int:
             raise TypeError("y must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("y must be >= 0")
-        self.__y = value
+        else:
+            self.__y = value
 
-    def update(self, *args, **kwargs):
-        """Update values of all parameters"""
-        if args and args != 0:
-            position = 0
-            for arg in args:
-                if position == 0:
-                    self.id = arg
-                elif position == 1:
-                    self.width = arg
-                elif position == 2:
-                    self.height = arg
-                elif position == 3:
-                    self.x = arg
-                elif position == 4:
-                    self.y = arg
-                else:
-                    return
-                position += 1
-        elif kwargs and len(kwargs) != 0:
-            for i, j in kwargs.items():
-                if i == "id":
-                    self.id = j
-                elif i == "width":
-                    self.width = j
-                elif i == "height":
-                    self.height = j
-                elif i == "x":
-                    self.x = j
-                elif i == "y":
-                    self.y = j
+    def area(self):
+        """Method that returns the area of the rectangle object
+        Args:
+           Not arguments
+        Return:
+           Area of the rectangle object
+        """
+        return self.width * self.height
+
+    def display(self):
+        """Method that prints to stdout
+           Rectangle object with the character #
+        """
+        for i in range(self.y):
+            print()
+        for i in range(self.height):
+            print(" " * self.x + self.width * '#')
 
     def __str__(self):
-        """Return [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
+        """Method that override str method
+        """
         return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x,
                                                         self.y, self.width,
                                                         self.height))
 
+    def update(self, *args, **kwargs):
+        """Method that changed the order of arguments for rectangle object
+        Args:
+           *args: list of arguments
+           **kwargs: Dictionary with arguments
+        Return:
+           Always nothing
+        """
+        dict_order = ['id', 'width', 'height', 'x', 'y']
+        if args is not None and bool(args) is True:
+            i = 0
+            for key in dict_order:
+                try:
+                    setattr(self, key, args[i])
+                except IndexError:
+                    pass
+                i += 1
+        else:
+            for key in dict_order:
+                try:
+                    setattr(self, key, kwargs[key])
+                except KeyError:
+                    pass
+
     def to_dictionary(self):
-        """Dictionary for attributes and values"""
-        return {"id": self.id, "height": self.height, "width": self.width,
-                "x": self.x, "y": self.y}
+        """Method that returns a dictionary with
+           attributes of the object.
+        """
+        dict_order = ['x', 'y', 'id', 'height', 'width']
+        dict_attrs = {}
+        for key in dict_order:
+            dict_attrs[key] = getattr(self, key)
+        return dict_attrs
