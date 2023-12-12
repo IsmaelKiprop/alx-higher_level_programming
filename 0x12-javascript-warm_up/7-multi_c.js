0182a1fc875367cf8f847a092470a9bd06f5c815
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2023-12-12 20:49:38 +0300

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
+const x = process.argv[2];
 
-const x = parseInt(process.argv[2]);
-if (x) {
-  let i = 0;
-  while (i < x) {
-    console.log('C is fun');
-    i++;
-  }
-} else {
+if (!parseInt(x)) {
   console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
