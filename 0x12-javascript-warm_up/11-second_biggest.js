f022d4ae2e1cfa883afbfa089f234b25a3388b98
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2023-12-12 20:57:57 +0300

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-
-if (process.argv.length < 4) {
-  console.log('0');
+if (process.argv.length <= 3) {
+  console.log(0);
 } else {
-  const arg = process.argv.slice(2);
-  arg.sort(function (a, b) { return b - a; });
-  console.log(arg[1]);
+  const args = process.argv
+    .map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
